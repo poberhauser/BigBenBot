91f011e7df24f28f411d3e53d36c032b5f30faa0
Patrick Oberhauser <poberhause@gmail.com>
2021-06-30 19:19:53 -0500

    updated avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,11 +3,11 @@ module.exports = {
     description: 'displays avatar (no, not the movie)',
     execute(message, args) {
         if (!message.mentions.users.size) {
-            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
         }
 
         const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ dynamic: true })}>`;
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
         });
 
         message.channel.send(avatarList);
